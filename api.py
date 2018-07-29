230d71f0b715c6306712d8dd5eddbb44cf55a00d
zyabxwcd <zyabxwcd@gmail.com>
2018-07-29 22:30:29 +0530

    Update api.py

--- api.py
+++ api.py
@@ -15,18 +15,23 @@ def index():
 def predict():
 		game_size = request.form['game_size']
 		party_size = request.form['party_size']
-		player_assists = request.form['player_assists']
 		knockdowns = request.form['player_dbno']
 		dist_walk = request.form['player_dist_walk']
 		hitpoint = request.form['player_dmg']
 		player_kills = request.form['player_kills']
 		survival_time = request.form['player_survival_time']
-		parameters = np.array([game_size,party_size,player_assists,knockdowns,dist_walk,hitpoint,player_kills,survival_time])
-		prediction = model.predict(parameters.reshape(1, -1))
-		return render_template('index.html', label = prediction)
+		parameters = np.array([game_size,party_size,knockdowns,dist_walk,hitpoint,player_kills,survival_time])
+		prediction = model1.predict(parameters.reshape(1, -1))
+		if (prediction == -1):
+			return render_template('index.html', label = "You're an amateur! Play more to be amongst the top 10.")
+		else :
+			prediction = model2.predict_proba(parameters.reshape(1,-1))
+			label = "You're an elite amongst the top 10. Rank Probability :" + str(prediction)
+			return render_template('index.html', label = label)
 	
 if __name__ == '__main__':
 	# load ml model
-	model = joblib.load('finalized_model.pkl')
+	model1 = joblib.load('top10_Pred.pkl')
+	model2 = joblib.load('top10_ProbPred.pkl')
 	# start api
 	app.run(port=8000, debug=True)
