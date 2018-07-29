6d92f0e38b4a751653f7cb5c85526336dca55087
zyabxwcd <zyabxwcd@gmail.com>
2018-07-29 23:51:20 +0530

    Update api.py

--- api.py
+++ api.py
@@ -23,15 +23,15 @@ def predict():
 		parameters = np.array([game_size,party_size,knockdowns,dist_walk,hitpoint,player_kills,survival_time])
 		prediction = model1.predict(parameters.reshape(1, -1))
 		if (prediction == -1):
-			return render_template('index.html', label = "You're an amateur! Play more to be amongst the top 10.")
+			return render_template('index.html', label = "You're an amateur! Play more, improve your game to be amongst the top 10.")
 		else:
 			prediction = model2.predict_proba(parameters.reshape(1,-1))
-			label = "You're an elite amongst the top 10. Rank Probability :" + str(prediction)
+			label = "You're an amongst the top 10. Rank Probabilities :" + str(prediction)
 			return render_template('index.html', label = label)
 	
 if __name__ == '__main__':
 	# load ml model
-	model1 = joblib.load('top10_Pred.pkl')
-	model2 = joblib.load('top10_ProbPred.pkl')
+	model1 = joblib.load('top10_Prediction.pkl')
+	model2 = joblib.load('top10_Probabilities.pkl')
 	# start api
 	app.run(port=8000, debug=True)
