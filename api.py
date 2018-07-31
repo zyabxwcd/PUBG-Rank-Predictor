77468f3cd7337fecf9fbf7b6f6265c4359bdb653
zyabxwcd <zyabxwcd@gmail.com>
2018-08-01 00:25:09 +0530

    Update api.py

--- api.py
+++ api.py
@@ -13,25 +13,29 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-		game_size = request.form['game_size']
 		party_size = request.form['party_size']
-		knockdowns = request.form['player_dbno']
+		dist_ride = request.form['player_dist_ride']
 		dist_walk = request.form['player_dist_walk']
 		hitpoint = request.form['player_dmg']
-		player_kills = request.form['player_kills']
-		survival_time = request.form['player_survival_time']
-		parameters = np.array([game_size,party_size,knockdowns,dist_walk,hitpoint,player_kills,survival_time])
-		prediction = model1.predict(parameters.reshape(1, -1))
+		parameters = np.array([party_size,dist_ride,dist_walk,hitpoint]).reshape(1,-1)
+		prediction = top10.predict(parameters)
 		if (prediction == -1):
-			return render_template('index.html', label = "You're an amateur! Play more, improve your game to be amongst the top 10.")
-		else:
-			prediction = model2.predict_proba(parameters.reshape(1,-1))
-			label = "You're an amongst the top 10. Rank Probabilities :" + str(prediction)
+			return render_template('index.html', label = "You're an amateur, not even in top 10! Play more, improve your game.")
+		else :
+			prediction = top3.predict(parameters)
+			if(prediction == 1):
+				label = "You'll be amongst the top 3. You're an elite."
+			else:
+				if(top5.predict(parameters) == 1):
+					label = "You'll be amongst the top 5. Good Game."
+				else:
+					label = "You'll be amongst the top 10. Try Harder."
 			return render_template('index.html', label = label)
 	
 if __name__ == '__main__':
-	# load ml model
-	model1 = joblib.load('top10_Prediction.pkl')
-	model2 = joblib.load('top10_Probabilities.pkl')
+	# load ml models
+	top10 = joblib.load('top10.pkl')
+	top5 = joblib.load('top5.pkl')
+	top3 = joblib.load('top3.pkl')
 	# start api
 	app.run(port=8000, debug=True)
